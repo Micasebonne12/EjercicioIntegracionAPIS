8036cf596abf942cc379ec37e485c8f57cea2c69
Micaela Casebonne <micaela.casebonne@arbusta.net>
2022-07-20 17:42:38 -0300

    Comento el codigo, agrego favicon y cambio estilos

--- js/apis.js
+++ js/apis.js
@@ -1,23 +1,29 @@
+/* Function cuando el documento esté cargado hace una petición GET con AJAX a la URL */
 $(document).ready(function () {
     $.ajax({
         url: "https://dog.ceo/api/breeds/list",
         method: 'GET',
         dataType: 'json',
+        /* Si recibe la respuesta correctamente, ocurre la función success */
         success: function (data) {
+            /* Recorre los datos que encuentra, captura el id razas (un div en el HTML) y le agrega para cada ítem un boton con un evento*/
             data.message.forEach(function (raza) {
-                $('#razas').append(`<li class="list-group-item justify-content-between align-items-center" onclick="verImg(event, '${raza}')">
-                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#modalButton">
+                $('#razas').append(`<button type="button" onclick="verImg(event, '${raza}')" class="btn btn-danger text-white" style="margin-bottom: 20px" data-bs-toggle="modal" data-bs-target="#modalButton">
                         ${raza}
-                      </button> </li>`);
+                      </button>`);
             })
         }
     });
 
+    /*La función verImg toma como parametro un evento y la raza*/
     verImg = (e, raza) => {
+        /* Hace un console.log del evento */
         console.log(e);
+        /* Petición GET con AJAX a la URL donde estan las imagenes, siendo la pasada por parametro la que aparezca*/
         $.ajax({
             url: `https://dog.ceo/api/breed/${raza}/images/random`,
             method: 'GET',
+            /* Si la petición sale bien, escribe como título del modal, el nombre del perro clickeado y la imagen correspondiente a ese perrito*/
             success: function (data) {
                 $('#titleModalLabel').html(`${raza}`)
                 $('#imagen').attr('src', data.message)
