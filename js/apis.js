3fa975313e5f43934e023abb7dffea01af86c2e6
Micaela Casebonne <micaela.casebonne@arbusta.net>
2022-07-21 17:20:56 -0300

    Modifico funcion, quitando parametro de evento innecesario

--- js/apis.js
+++ js/apis.js
@@ -6,19 +6,17 @@ $(document).ready(function () {
         dataType: 'json',
         /* Si recibe la respuesta correctamente, ocurre la función success */
         success: function (data) {
-            /* Recorre los datos que encuentra, captura el id razas (un div en el HTML) y le agrega para cada ítem un boton con un evento*/
+            /* Recorre los datos que encuentra, captura el id razas (un div en el HTML) y le agrega para cada ítem un boton con un evento que devuelve el valor del elemento clickeado*/
             data.message.forEach(function (raza) {
-                $('#razas').append(`<button type="button" onclick="verImg(event, '${raza}')" class="btn btn-danger text-white" style="margin-bottom: 20px" data-bs-toggle="modal" data-bs-target="#modalButton">
+                $('#razas').append(`<button type="button" onclick="verImg('${raza}')" class="btn btn-danger text-white" style="margin-bottom: 20px" data-bs-toggle="modal" data-bs-target="#modalButton">
                         ${raza}
                       </button>`);
             })
         }
     });
 
-    /*La función verImg toma como parametro un evento y la raza*/
-    verImg = (e, raza) => {
-        /* Hace un console.log del evento */
-        console.log(e);
+    /*La función verImg toma como parametro la raza*/
+    verImg = (raza) => {
         /* Petición GET con AJAX a la URL donde estan las imagenes, siendo la pasada por parametro la que aparezca*/
         $.ajax({
             url: `https://dog.ceo/api/breed/${raza}/images/random`,
